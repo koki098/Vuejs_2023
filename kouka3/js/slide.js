ebae4d8aceeeae5ae991a1ddc1bd1dc48a5aa8da
koki-hirosawa <hirosawa_fam_05@icloud.com>
2023-06-07 11:03:19 +0900

    効果測定３回

--- kouka3/js/slide.js
+++ kouka3/js/slide.js
@@ -6,7 +6,6 @@ $('.slider').slick({
     infinite: true,//スライドをループさせるかどうか。初期値はtrue。
     slidesToShow: 1,//スライドを画面に3枚見せる
     slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
-    arrows: true,//左右の矢印あり
     prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
     nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
         pauseOnFocus: false,//フォーカスで一時停止を無効
